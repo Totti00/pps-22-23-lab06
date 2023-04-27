600ed0a5f3b8bd1f96170f239d05c96e57a7dfc8
Totti00 <gek5800@gmail.com>
2023-04-27 17:12:08 +0200

    refactor

--- src/main/scala/u06lab/solution/Solitaire.scala
+++ src/main/scala/u06lab/solution/Solitaire.scala
@@ -4,16 +4,16 @@ object Game:
   type Position = (Int, Int)
   def Board(width: Int, height: Int): Solitaire = new SolitaireImpl(width, height)
   trait Solitaire:
-    def placeMarks(size: Int): Seq[Iterable[Position]]
+    def placeMarks(size: Int, n: Int): Seq[Iterable[Position]]
     def render(solution: Iterable[Position]): String
 
   class SolitaireImpl(width: Int, height: Int) extends Solitaire:
 
-    override def placeMarks(size: Int): Seq[Iterable[Position]] = size match
+    override def placeMarks(size: Int, n: Int): Seq[Iterable[Position]] = n match
       case 1 => List(Set((width / 2, height / 2)))
       case _ =>
         for
-          board <- placeMarks(size - 1)
+          board <- placeMarks(size, n - 1)
           x <- 0 until width
           y <- 0 until height
           cell = (x, y)
@@ -32,13 +32,18 @@ object Game:
       rows.mkString("\n")
 
     def legalMove(pos: Position, move: Position): Boolean =
-      ((pos._1 - move._1).abs + (pos._2 - move._2).abs) == 2
+      (math.abs(pos._1 - move._1) == 2 && math.abs(pos._2 - move._2) == 2)
+        || (math.abs(pos._1 - move._1) == 3 && math.abs(pos._2 - move._2) == 0)
+        || (math.abs(pos._1 - move._1) == 0 && math.abs(pos._2 - move._2) == 3)
 
 
 @main def testSolitaire(): Unit =
   import Game.*
-  val board = Board(3, 3)
-  val solutions = board.placeMarks(5)
+  val height = 3;
+  val width = 3;
+  val size = height * width;
+  val board = Board(width, height)
+  val solutions = board.placeMarks(size, 9)
   solutions foreach (sol => println(board.render(sol) + "\n"))
 
   println("There are " + solutions.size + " solutions.")
